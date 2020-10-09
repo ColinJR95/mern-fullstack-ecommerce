806435730d155a5a2db039c7fce5e9dfdea82c7f
Javs C <javcolin1@gmail.com>
2020-10-08 19:10:50 -0700

    updating

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDw92Ru7oVbvjOYxvZH61uRrf9JEb25ljs",
+  authDomain: "mern-ecommerce-f7e99.firebaseapp.com",
+  databaseURL: "https://mern-ecommerce-f7e99.firebaseio.com",
+  projectId: "mern-ecommerce-f7e99",
+  storageBucket: "mern-ecommerce-f7e99.appspot.com",
+  messagingSenderId: "1025538116419",
+  appId: "1:1025538116419:web:e6cded951ec8d7a45c7df6",
+  measurementId: "G-DF00BCBDQL"
+};
