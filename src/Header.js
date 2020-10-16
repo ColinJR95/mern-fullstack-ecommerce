4636f44fd245dc5f673cdada1c2281e5026cc315
Javs C <javcolin1@gmail.com>
2020-10-16 03:19:00 -0700

    changing header comp

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,51 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
 	return (
 		<div className='header'>
-			<img src='https://www.javiercolin.com/wp-content/uploads/2020/10/cropped-Full-Logo-Black-300x74.png' alt ="this is our logo not loading"/>
+			<img className="header_logo" src='https://www.javiercolin.com/wp-content/uploads/2019/01/Full-Logo-Color-2014x500.png' alt ="this is our logo not loading"/>
 
 			<div className="header_search">
 				<input className="header_searchInput" type="text" />
-			{  /*logo */ }
-				this is the search component
+			<SearchIcon className="header_searchIcon" />
+			</div>
+
+			<div className="header_nav">
+				<div className="nav_options">
+						<span className="nav_optionLineOne">
+							Hello Guest 
+						</span>
+						<span className="nav_optionLineOne">
+							Sign In 
+						</span>
+				</div>
+
+				<div className="nav_options">
+				<span className="nav_optionLineOne">
+							Returns
+						</span>
+						<span className="nav_optionLineOne">
+							& Orders
+						</span>
+				</div>
+
+				<div className="nav_options">
+				<span className="nav_optionLineOne">
+							Your 
+						</span>
+						<span className="nav_optionLineOne">
+							Account
+						</span>
+				</div>
+				 
+
+				 <div className="nav_options">
+				 cart
+				</div>
+
+
 			</div>
 		</div>
 	)
