fc89e9d79d504400f4164f2c792329bbfe61a600
Javs C <javcolin1@gmail.com>
2020-10-16 23:29:06 -0700

    changing nav css

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ import SearchIcon from '@material-ui/icons/Search';
 function Header() {
 	return (
 		<div className='header'>
-			<img className="header_logo" src='https://www.javiercolin.com/wp-content/uploads/2019/01/Full-Logo-Color-2014x500.png' alt ="this is our logo not loading"/>
+			<img className="header_logo" src='https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/120848646_120240026502211_2460028825837157574_o.png?_nc_cat=101&_nc_sid=09cbfe&_nc_ohc=jwJ1HMcQwVQAX9h8Hi9&_nc_ht=scontent-lax3-1.xx&oh=197c20a16dec457f09e806fd9402cc2d&oe=5FAE79C9' />
 
 			<div className="header_search">
 				<input className="header_searchInput" type="text" />
